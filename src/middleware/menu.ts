2ab29508e51717ef851e35d5fb9449dd5ab0a580
DDeenis <ddeenis46@gmail.com>
2021-07-02 21:15:01 +0300

    add schedule for month

--- src/middleware/menu.ts
+++ src/middleware/menu.ts
@@ -1,12 +1,13 @@
 import { MenuMiddleware, MenuTemplate } from "telegraf-inline-menu";
 import { Context } from "vm";
 import homeworkSubmenu from "./submenu/homework.js";
-import { scheduleTodaySubmenu, scheduleTomorrowSubmenu } from "./submenu/schedule.js";
+import { monthScheduleSubmenu, scheduleTodaySubmenu, scheduleTomorrowSubmenu } from "./submenu/schedule.js";
 
 const menuTemplate = new MenuTemplate<Context>(() => 'Выберите действие');
 
 menuTemplate.submenu('Расписание на сегодня', 'schedule-today', scheduleTodaySubmenu);
 menuTemplate.submenu('Расписание на завтра', 'schedule-tomorrow', scheduleTomorrowSubmenu);
+menuTemplate.submenu('Расписание на месяц', 'schedule-month', monthScheduleSubmenu);
 menuTemplate.submenu('Домашние задания', 'hw', homeworkSubmenu);
 
 const menuMiddleware = new MenuMiddleware('menu/', menuTemplate);
--- src/middleware/submenu/schedule.ts
+++ src/middleware/submenu/schedule.ts
@@ -1,19 +1,22 @@
 import { getScheduleByDate } from "mystat-api";
 import { createBackMainMenuButtons, MenuTemplate } from "telegraf-inline-menu";
 import { Context } from "vm";
-import { MystatResponse } from "../../types.js";
 import { formatMessage, getUserDataFromSession } from "../../utils.js";
 
-const scheduleTodaySubmenu = new MenuTemplate<Context>(async (ctx) => {
-    const todayDate = new Date();
+const getScheduleFormatted = async (ctx: Context, title: string, day?: number): Promise<string> => {
+    const date = new Date();
 
-    const schedule: MystatResponse = await getScheduleByDate(getUserDataFromSession(ctx), todayDate);
+    if (day) {
+        date.setDate(day);
+    }
+
+    const schedule = await getScheduleByDate(getUserDataFromSession(ctx), date);
     let scheduleFormatted = '';
 
     if (!schedule.success) {
         return '🚫 При получении расписания возникла ошибка: ' + schedule.error;
-    } else if (schedule.data.length <= 0) {
-        return '🎉 У вас сегодня нет пар';
+    } else if (schedule.data.length === 0) {
+        return '🎉 Нет пар';
     }
 
     for (const scheduleEntry of schedule.data) {
@@ -26,42 +29,42 @@ const scheduleTodaySubmenu = new MenuTemplate<Context>(async (ctx) => {
     }
 
     return [
-        'Раписание на сегодня\n',
+        title + '\n',
         scheduleFormatted
     ].join('\n');
-});
+}
+
+const getDateString = (date: Date = new Date()) => date.toLocaleString().substring(3, 10);
+const daysInMonth = (year: number, month: number): number => new Date(year, month, 0).getDate();
+const getDaysArray = (date: Date): number[] => {
+    const totalDays = daysInMonth(date.getFullYear(), date.getMonth() + 1);
+    const days: number[] = [];
+
+    for (let index = 0; index < totalDays; index++) {
+        days.push(index + 1);
+    }
+
+    return days;
+}
+
+const scheduleTodaySubmenu = new MenuTemplate<Context>(async (ctx) => await getScheduleFormatted(ctx, 'Раписание на сегодня'));
 scheduleTodaySubmenu.manualRow(createBackMainMenuButtons('⬅️ Назад'));
 
-const scheduleTomorrowSubmenu = new MenuTemplate<Context>(async (ctx) => {
-    const tomorrowDate = new Date();
-    tomorrowDate.setDate(tomorrowDate.getDate() + 1);
-
-    const schedule: MystatResponse = await getScheduleByDate(getUserDataFromSession(ctx), tomorrowDate);
-    let scheduleFormatted = '';
-
-    if (!schedule.success) {
-        return '🚫 При получении расписания возникла ошибка: ' + schedule.error;
-    } else if (schedule.data.length <= 0) {
-        return '🎉 У вас завтра нет пар';
-    }
-
-    for (const scheduleEntry of schedule.data) {
-        scheduleFormatted += formatMessage(
-            `✏️ Предмет: ${scheduleEntry.subject_name}`,
-            `💡 Преподаватель: ${scheduleEntry.teacher_name}`,
-            `🗝 Аудитория: ${scheduleEntry.room_name}`,
-            `⏰ Время: ${scheduleEntry.started_at} - ${scheduleEntry.finished_at}`,
-        );
-    }
-
-    return [
-        'Раписание на завтра\n',
-        scheduleFormatted
-    ].join('\n');
-});
+const scheduleTomorrowSubmenu = new MenuTemplate<Context>(async (ctx) => await getScheduleFormatted(ctx, 'Раписание на завтра', (new Date().getDate() + 1)));
 scheduleTomorrowSubmenu.manualRow(createBackMainMenuButtons('⬅️ Назад'));
+
+const monthScheduleEntrySubmenu = new MenuTemplate<Context>(async (ctx) => {
+    const day = ctx.match[1];
+    return await getScheduleFormatted(ctx, `Расписание на ${day}.${getDateString()}`, parseInt(day));
+});
+monthScheduleEntrySubmenu.manualRow(createBackMainMenuButtons('⬅️ Назад'));
+
+const monthScheduleSubmenu = new MenuTemplate<Context>(() => `Расписание на ${getDateString()}`);
+monthScheduleSubmenu.chooseIntoSubmenu('schedule-month-days', getDaysArray(new Date()), monthScheduleEntrySubmenu, { columns: 7 });
+monthScheduleSubmenu.manualRow(createBackMainMenuButtons('⬅️ Назад'));
 
 export {
     scheduleTodaySubmenu,
-    scheduleTomorrowSubmenu
+    scheduleTomorrowSubmenu,
+    monthScheduleSubmenu
 }
