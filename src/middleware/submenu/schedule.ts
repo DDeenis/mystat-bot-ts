c03707bb52cc25c68ab85981664941014fc933f6
BeloMaximka <belomaxim@gmail.com>
2021-09-09 22:43:41 +0300

    added month schedule marks

--- src/middleware/submenu/schedule.ts
+++ src/middleware/submenu/schedule.ts
@@ -1,4 +1,4 @@
-import { getScheduleByDate } from "mystat-api";
+import { getScheduleByDate, getMonthSchedule } from "mystat-api";
 import telegraf_inline from "telegraf-inline-menu";
 import { Context } from "vm";
 import { formatMessage, getUserDataFromSession } from "../../utils.js";
@@ -39,14 +39,24 @@ const getScheduleFormatted = async (ctx: Context, title: string, day?: number): 
 
 const getDateString = (date: Date = new Date()) => date.toLocaleString().substring(3, 10);
 const daysInMonth = (year: number, month: number): number => new Date(year, month, 0).getDate();
-const getDaysArray = (date: Date): string[] => {
+const getDaysArray = async (date: Date, ctx: Context): Promise<string[]> => {
     const totalButtons = 35;
     const totalDays = daysInMonth(date.getFullYear(), date.getMonth() + 1);
     const days: string[] = [];
 
+    const schedule = await getMonthSchedule(getUserDataFromSession(ctx), date);
     // actual buttons
+    let date_template = date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-'; // smth like '2000-02-'
     for (let count = 0; count < totalDays; count++) {
-        days.push(String(count + 1));
+
+        date.setDate(count + 1);
+        let cur_date = date_template + ('0' + (count + 1)).slice(-2); // smth like '2000-02-' + '02'
+        if (schedule.data.find(elem => elem.date === cur_date) === undefined) { // if no lession matches with date
+            days.push('🔴' + String(count + 1)); // put red dot
+        }
+        else {
+            days.push('🟢' + String(count + 1)); // put green got
+        }
     }
 
     // empty buttons
@@ -64,7 +74,8 @@ const scheduleTomorrowSubmenu = new MenuTemplate<Context>(async (ctx) => await g
 scheduleTomorrowSubmenu.manualRow(createBackMainMenuButtons('⬅️ Назад'));
 
 const monthScheduleEntrySubmenu = new MenuTemplate<Context>(async (ctx) => {
-    const day = ctx.match[1];
+    const day = ctx.match[1].match(/\d+/)[0];
+    console.log(day);
 
     if (day === ' ') {
         return '🎉 Нет пар';
@@ -75,7 +86,7 @@ const monthScheduleEntrySubmenu = new MenuTemplate<Context>(async (ctx) => {
 monthScheduleEntrySubmenu.manualRow(createBackMainMenuButtons('⬅️ Назад'));
 
 const monthScheduleSubmenu = new MenuTemplate<Context>(() => `Расписание на ${getDateString()}`);
-monthScheduleSubmenu.chooseIntoSubmenu('schedule-month-days', getDaysArray(new Date()), monthScheduleEntrySubmenu, { columns: 7 });
+monthScheduleSubmenu.chooseIntoSubmenu('schedule-month-days', async (ctx) => await getDaysArray(new Date(), ctx), monthScheduleEntrySubmenu, { columns: 7 });
 monthScheduleSubmenu.manualRow(createBackMainMenuButtons('⬅️ Назад'));
 
 export {
