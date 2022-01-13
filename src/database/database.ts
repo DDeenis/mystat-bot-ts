5310045921ad430bcbe551cbe7a31b0c61498426
DDeenis <ddeenis46@gmail.com>
2022-01-13 12:53:44 +0200

    ensure connection

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,5 +1,6 @@
-import mongoose from 'mongoose';
-import {IUser, UserModel} from './entity/User.js';
+import mongoose from "mongoose";
+import { IUser, UserModel } from "./entity/User.js";
+import dotenv from "dotenv";
 
 export const connectMongo = async (connectionString: string): Promise<void> => {
   try {
@@ -12,22 +13,27 @@ export const connectMongo = async (connectionString: string): Promise<void> => {
         useNewUrlParser: true,
         useFindAndModify: false,
       },
-      (err) => console.log(err ? err : 'Mongoose is connected'),
+      (err) => console.log(err ? err : "Mongoose is connected")
     );
   } catch (error) {
-    console.log('Failed to connect mongoose', error);
+    console.log("Failed to connect mongoose", error);
   }
 };
 
 export const createUser = async (user: IUser): Promise<void> => {
   try {
-    await UserModel.findOneAndUpdate({chatId: user.chatId}, user, {upsert: true}, (err) => {
-      if (err) {
-        console.log('Error while creating/updating user: ' + err);
-      } else {
-        console.log('User created/updated successfully');
+    await UserModel.findOneAndUpdate(
+      { chatId: user.chatId },
+      user,
+      { upsert: true },
+      (err) => {
+        if (err) {
+          console.log("Error while creating/updating user: " + err);
+        } else {
+          console.log("User created/updated successfully");
+        }
       }
-    });
+    );
   } catch (error) {
     console.log(error);
   }
@@ -35,14 +41,20 @@ export const createUser = async (user: IUser): Promise<void> => {
 
 export const deleteUser = async (chatId: number): Promise<void> => {
   try {
-    await UserModel.deleteOne({chatId});
+    await UserModel.deleteOne({ chatId });
   } catch (error) {
     console.log(error);
   }
 };
 
-export const getUserByChatId = async (chatId: number): Promise<IUser | undefined> => {
-  return (await UserModel.findOne({chatId}))?.toObject();
+export const getUserByChatId = async (
+  chatId: number
+): Promise<IUser | undefined> => {
+  if (mongoose.connections.length < 1) {
+    dotenv.config();
+    await connectMongo(process.env?.BOT_TOKEN ?? "");
+  }
+  return (await UserModel.findOne({ chatId }))?.toObject();
 };
 
 export const isUserExist = async (chatId: number): Promise<boolean> => {
