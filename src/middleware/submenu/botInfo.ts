cdb9f16ad838fd483517b05abf91154544a1c033
DDeenis <ddeenis46@gmail.com>
2022-08-29 16:45:44 +0300

    add multiple usernames

--- src/middleware/submenu/botInfo.ts
+++ src/middleware/submenu/botInfo.ts
@@ -2,6 +2,8 @@ import { Scenes } from "telegraf";
 import telegraf_inline from "telegraf-inline-menu";
 import { formatMessage } from "../../utils.js";
 
+const tgUsernames = ["@ddeenis", "@BeloMaximka"];
+
 const createBackMainMenuButtons = telegraf_inline.createBackMainMenuButtons;
 const MenuTemplate = telegraf_inline.MenuTemplate;
 
@@ -10,7 +12,7 @@ const botInfoSubmenu = new MenuTemplate<Scenes.WizardContext>(async () => {
     "Бот создан для легкого доступа к расписанию и другим функциям mystat из телеграма. Код бота открытый, если вы не доверяете конкретно этому боту, можете скачать код с github и захостить своего бота.",
     "",
     "Github: https://github.com/DDeenis/mystat-bot-ts",
-    "По вопросам и предложениям писать @ddeenis"
+    `По вопросам писать ${tgUsernames.join(" или ")}`
   );
 
   return {
