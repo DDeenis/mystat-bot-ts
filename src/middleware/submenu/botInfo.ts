51282608e79aed23843e999bdd8bebddd15f0f04
DDeenis <ddeenis46@gmail.com>
2022-08-29 17:36:39 +0300

    add warning

--- src/middleware/submenu/botInfo.ts
+++ src/middleware/submenu/botInfo.ts
@@ -10,6 +10,7 @@ const MenuTemplate = telegraf_inline.MenuTemplate;
 const botInfoSubmenu = new MenuTemplate<Scenes.WizardContext>(async () => {
   const infoFromatted = formatMessage(
     "Бот создан для легкого доступа к расписанию и другим функциям mystat из телеграма. Код бота открытый, если вы не доверяете конкретно этому боту, можете скачать код с github и захостить своего бота.",
+    "Предупреждение: веб версия бота не работает в telegram web",
     "",
     "Github: https://github.com/DDeenis/mystat-bot-ts",
     `По вопросам писать ${tgUsernames.join(" или ")}`
